6dd9c54a3b59b4ade347491a572f1003fc803279
admin <admin@admin-PC>
2020-02-16 18:09:43 +0800

    QQ

--- src/cn/com/Test.java
+++ src/cn/com/Test.java
@@ -2,7 +2,6 @@ package cn.com;
 
 public class Test {
     public static void main(String[] args) {
-		System.out.println("onlysssss");
-		System.out.println("Test.main()");
+		System.out.println("onlyssss");
 	}
 }
