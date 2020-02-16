6bee16ed28248ea914a733f199c8e0812950d3cb
admin <admin@admin-PC>
2020-02-16 21:45:22 +0800

    11

--- src/cn/com/Test.java
+++ src/cn/com/Test.java
@@ -5,6 +5,5 @@ public class Test {
 		    System.out.println("fuckyou");
 	        System.out.println("fuckyou");
 	        System.out.println("fuckyou");
-	        System.out.println("ssssssssssssssssssssss");
 	}
 }
