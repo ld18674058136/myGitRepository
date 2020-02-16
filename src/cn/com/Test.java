be6320e045335b81dd5f6efe4bb20b2a72b4431b
ld18674058136 <61084531+ld18674058136@users.noreply.github.com>
2020-02-16 21:21:07 +0800

    Update Test.java

--- src/cn/com/Test.java
+++ src/cn/com/Test.java
@@ -2,8 +2,8 @@ package cn.com;
 
 public class Test {
     public static void main(String[] args) {
-		    System.out.println("fuck you");
-	        System.out.println("fuck you");
-	        System.out.println("fuck you");
+		    System.out.println("fuckyou");
+	        System.out.println("fuckyou");
+	        System.out.println("fuckyou");
 	}
 }
