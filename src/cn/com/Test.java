f74f17f48b3956a37b5be473a6515560c5d5531d
admin <admin@admin-PC>
2020-02-17 15:18:03 +0800

    ff

--- src/cn/com/Test.java
+++ src/cn/com/Test.java
@@ -2,10 +2,7 @@ package cn.com;
 
 public class Test {
     public static void main(String[] args) {
-		System.out.println("fuck 有");
-            System.out.println("fuck 有");
-	    System.out.println("fuck 有");
-	    System.out.println("fuck 有");
+		  System.out.println("Test.main()");
 		   
 	}
 }
