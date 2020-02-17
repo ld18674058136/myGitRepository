11fa625fe0a7b8eb4e6463064bec7b2dd82b0a12
ld18674058136 <61084531+ld18674058136@users.noreply.github.com>
2020-02-17 16:51:48 +0800

    Update Test.java

--- src/cn/com/Test.java
+++ src/cn/com/Test.java
@@ -2,8 +2,8 @@ package cn.com;
 
 public class Test {
     public static void main(String[] args) {
-		  System.out.println("ko");
-	    		  System.out.println("ko");
+		  System.out.println("ok");
+	    		  System.out.println("ok");
 
 		   
 	}
