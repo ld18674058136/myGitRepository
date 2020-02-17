250c710e0381b26d189a6f7a499cfaf5f06ef7ca
admin <admin@admin-PC>
2020-02-17 15:19:46 +0800

    d

--- src/cn/com/Test.java
+++ src/cn/com/Test.java
@@ -2,7 +2,8 @@ package cn.com;
 
 public class Test {
     public static void main(String[] args) {
-		  System.out.println("Test.main()");
+		  System.out.println("Test you");
+		  System.out.println("Test you");
 		   
 	}
 }
