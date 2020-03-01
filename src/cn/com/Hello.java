f9299f215264e2b4e4ede8ecaae779f815c28feb
liudeng <admin@admin-PC>
2020-03-01 18:53:16 +0800

    还原

--- src/cn/com/Hello.java
+++ src/cn/com/Hello.java
@@ -5,8 +5,7 @@ public class Hello {
     public static void main(String[] args) {
 		System.out.println("no");
 		System.out.println("no");		
-		System.out.println("no    ");		
-        System.out.println("Hello.main()");
+		System.out.println("no");		
 
 
 
