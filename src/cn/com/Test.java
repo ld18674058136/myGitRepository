1bc814803224f5d5c9b8f72c2a422cd66accc340
ld18674058136 <61084531+ld18674058136@users.noreply.github.com>
2020-03-01 19:37:56 +0800

    Update Test.java

--- src/cn/com/Test.java
+++ src/cn/com/Test.java
@@ -2,8 +2,8 @@ package cn.com;
 
 public class Test {
     public static void main(String[] args) {
-		  System.out.println();
-		  System.out.println();
-		  System.out.println();
+		  System.out.println("");
+		  System.out.println("");
+		  System.out.println("");
 	}
 }
