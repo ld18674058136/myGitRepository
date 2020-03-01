64b60f5d3137603e2ecdabcace688a97136c33f3
ld18674058136 <61084531+ld18674058136@users.noreply.github.com>
2020-03-01 18:29:41 +0800

    Update Hello.java

--- src/cn/com/Hello.java
+++ src/cn/com/Hello.java
@@ -1,13 +1,13 @@
 package cn.com;
 
 public class Hello {
-	private String name="wangwu";
+	private String name="hh";
     public static void main(String[] args) {
 		System.out.println("no1");
 		System.out.println("no2");		
 		System.out.println("no3");		
 
-
+                System.out.println("no4");
 
 
 	}
