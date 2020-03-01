e300034eb853f6aa39abb8148f1c084471d28cc6
liudeng <admin@admin-PC>
2020-03-01 18:31:37 +0800

    都是

--- src/cn/com/Hello.java
+++ src/cn/com/Hello.java
@@ -1,12 +1,12 @@
 package cn.com;
 
 public class Hello {
-	private String name="wangwu";
+	private String name="vvvvv";
     public static void main(String[] args) {
-		System.out.println("no1");
-		System.out.println("no2");		
-		System.out.println("no3");		
-
+		System.out.println("no");
+		System.out.println("no");		
+		System.out.println("no");		
+        System.out.println("Hello.main()");
 
 
 
