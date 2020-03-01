5940a067e8a950e972e14fe56613ca15a77fc536
ld18674058136 <61084531+ld18674058136@users.noreply.github.com>
2020-03-01 15:33:57 +0800

    Update Hello.java

--- src/cn/com/Hello.java
+++ src/cn/com/Hello.java
@@ -2,6 +2,6 @@ package cn.com;
 
 public class Hello {
     public static void main(String[] args) {
-		System.out.println("第四次提交");
+		System.out.println("第四次提交                                    ");
 	}
 }
