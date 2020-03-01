410bbeb14c658dac8fdb7679a7213df7d076cb0a
ld18674058136 <61084531+ld18674058136@users.noreply.github.com>
2020-03-01 17:52:42 +0800

    Update Hello.java

--- src/cn/com/Hello.java
+++ src/cn/com/Hello.java
@@ -1,11 +1,13 @@
 package cn.com;
 
 public class Hello {
+	private String name="lisi";
+	
     public static void main(String[] args) {
 		System.out.println("no1");
 		System.out.println("no2");		
 		System.out.println("no3");		
-
+                System.out.println("no4");		
 
 
 
