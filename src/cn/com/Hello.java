baaf9f16f77c4b050d24ab148bcbed0974d77209
ld18674058136 <61084531+ld18674058136@users.noreply.github.com>
2020-03-01 22:12:17 +0800

    Update Hello.java

--- src/cn/com/Hello.java
+++ src/cn/com/Hello.java
@@ -2,9 +2,9 @@ package cn.com;
 
 public class Hello {
     public static void main(String[] args) {
-		System.out.println("");
-		System.out.println("");		
-		System.out.println("");		
+		System.out.println("a");
+		System.out.println("a");		
+		System.out.println("a");		
 
 
 
