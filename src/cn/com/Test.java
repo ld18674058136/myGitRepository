382110fe47c19a574f4e53345bee3bb57d331d4c
liudeng <admin@admin-PC>
2020-03-01 23:09:47 +0800

    上档次

--- src/cn/com/Test.java
+++ src/cn/com/Test.java
@@ -5,11 +5,7 @@ public class Test {
 		  System.out.println("Test_1");
 		  System.out.println("Test_1");
 		  System.out.println("Test_1");
-		  
-		  System.out.println("Test.main()");
-		  System.out.println("Test_develop");
-		  System.out.println("Test_develop");
-		  System.out.println("Test_develop");
+
 
 	}
 }
