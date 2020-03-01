9f70f65a1ed0d1018b02eb8fc7b4bec026664b63
ld18674058136 <61084531+ld18674058136@users.noreply.github.com>
2020-03-01 23:10:25 +0800

    Update Test.java

--- src/cn/com/Test.java
+++ src/cn/com/Test.java
@@ -2,9 +2,9 @@ package cn.com;
 
 public class Test {
     public static void main(String[] args) {
-		  System.out.println("Test_1");
-		  System.out.println("Test_1");
-		  System.out.println("Test_1");
+		  System.out.println("Test_1_a");
+		  System.out.println("Test_1_a");
+		  System.out.println("Test_1_a");
 
 
 	}
